c657debee25d884f34d5d43c7befd2793a9f709f
Elliot Cathcart <cathcart-e@ulster.ac.uk>
2020-01-05 23:38:46 +0000

    Solved removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
+    const numbers = arguments[0];
+    let newArray = [];
 
+    for (i = 0; i < numbers.length; i++) {
+      var contains = false;
+        for (j = 1; j < arguments.length; j++) {
+          if (numbers[i] === arguments[j]) {
+            contains = true;
+          }
+        }
+      if (!contains) {
+        newArray.push(numbers[i]);
+      }
+    }
+    return newArray;
 }
 
 module.exports = removeFromArray
